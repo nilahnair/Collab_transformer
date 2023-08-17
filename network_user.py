f5f7f79e455786e81a60c59f433f7a1f6b67e0c8
Nilah Ravi Nair <nilahnair.27@gmail.com>
2023-08-17 09:40:59 +0200

    correction to network path name

--- network_user.py
+++ network_user.py
@@ -25,7 +25,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from matplotlib.collections import PolyCollection
 
-from network_raphael import Network
+from network_rapahel import Network
 
 from HARWindows import HARWindows
 
